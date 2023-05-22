9ae205d3322bad7400b45992a93742baff50e4e5
chunillyum <otercc2@naver.com>
2023-05-22 17:26:45 +0900

    배열, 메소드

---
+++ Practice/src/array.java
@@ -0,0 +1,17 @@
+
+public class array {
+
+	public static void main(String[] args) {
+		String [] coffes = { "아메리카노", "콜드브루", "카페라떼"};//배열 선언과 동시에 값 입력
+//		for (int i=0; i < coffes.length; i++) { //length 는 배열의 크기를 알아볼 수 있음
+//			System.out.println(coffes[i]); //coffes라는 배열의 값을 i(length)로 꺼내옴
+//		}
+		//--------------------------------
+//		for (String e : coffes) {
+//			System.out.println(e + " 하나");//for each문, 
+//		}
+		//------------------------------------------
+		System.out.println(coffes[2]);
+	}
+
+}
---
+++ Practice/src/method.java
@@ -0,0 +1,20 @@
+
+public class method {
+//	public static String sayHello() {
+//		return ("Hi, I'm method");
+//	}
+//	
+//
+//	public static void main(String[] args) {
+//		System.out.println(sayHello()+" i'm groot");
+//	}
+
+	 public static void power(int number) {
+		 int result = number * number;
+		 System.out.println(number + "의 2승은 " + result);
+	 }
+	 public static void main(String[] args) {
+		 power(3);
+	 }
+	 
+}
