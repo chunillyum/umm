7eb998a4025393725b507ae88ccb547ae9005bd0
chunillyum <otercc2@naver.com>
2023-06-02 14:34:18 +0900

    ..

---
+++ Practice/src/javabook/areYouMyFriendChatGPT.java
@@ -0,0 +1,47 @@
+package javabook;//챗지피티가 짜준 버전
+//리턴해서 쓰는 버전임
+
+public class areYouMyFriendChatGPT {
+    static String info = "hi";
+
+    static class EleSchool {
+        static String[] getHometown() {
+            String[] hometown = {"hanbit", "hobin", "dongwoo", "dongman", "chanyang"};
+            return hometown;
+        }
+    }
+
+    static class MiddleSchool {
+        static String[] getHometown() {
+            String[] hometown = {"minjun", "jaewoong", "hyenwoo", "jaeho"};
+            return hometown;
+        }
+    }
+
+    static class HighSchool {
+        static String[] getHometown() {
+            String[] hometown = {"sicksick", "jaesung"};
+            return hometown;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] eleSchoolHometown = EleSchool.getHometown();
+        String[] middleSchoolHometown = MiddleSchool.getHometown();
+        String[] highSchoolHometown = HighSchool.getHometown();
+
+        System.out.println(info);
+
+        for (String friend : eleSchoolHometown) {
+            System.out.println(friend);
+        }
+
+        for (String friend : middleSchoolHometown) {
+            System.out.println(friend);
+        }
+
+        for (String friend : highSchoolHometown) {
+            System.out.println(friend);
+        }
+    }
+}
---
+++ Practice/src/javabook/arryEnter.java
@@ -0,0 +1,19 @@
+package javabook;
+
+public class arryEnter {
+	//지피티 코드 입맛대로 바꾸기
+	//class에 속한 배열의 값을 검사해서 입력값이 배열 속 값에 해당이 안 되면 ㅗ 를 출력
+
+	static class myLife{
+		static String[] getMyFriends() {
+			String[] eleFriends = {"hobin", "dongwoo"};
+			return eleFriends;
+		
+	}
+	
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
